d7f06adf05d3685c079a0df9702b859cafa521fb
manoj <manojpoojary2004@gmail.com>
2024-02-10 14:02:14 +0530

    first commit

---
+++ src/Component/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './style.css'
+
+export default function Navbar() {
+    return (
+        <nav className='navbar'>
+            <h2>Routines</h2><hr/>
+            <div className='navbar-container'>
+                <div>
+                    NEW
+                </div>
+            </div>
+        </nav>
+    )
+}
