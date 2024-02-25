957fcd4c864b0607b77561bec3876fd9036f1e94
manoj <manojpoojary2004@gmail.com>
2024-02-25 13:47:33 +0530

    alarm

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,26 +1,44 @@
-import React, { useEffect, useState } from 'react'
-import { NavLink } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import audio1 from './Audios/Ringtone.mp3'
+// import { NavLink } from 'react-router-dom';
 import './style.css'
 export default function Home() {
-    const [value, setvalue] = useState({
-        final_time: [],
-    });
+    const [time, setTime] = useState(null)
+    const [value, setvalue] = useState([])
+    const [label, setlabel] = useState([]);
+
+    let alertUser = () => {
+        let alarm = new Audio(audio1);
+        alarm.play();
+    }
+
+    let displayNotification = async (time, key) => {
+        if ("Notification" in window) {
+            let permission = await Notification.requestPermission();
+            if (permission === "granted") {
+                new Notification('alert', { body: `Alarm :${time} 
+                label : ${label[key]}` });
+                alertUser();
+            }
+            else {
+                alert("Not Supported");
+            }
+
+        }
+    }
+
     const handle_submit = () => {
         let hour_input = document.getElementById('set_hour');
         let min_input = document.getElementById('set_min');
-        setvalue(prevalue => ({
-            ...value,
-            final_time: [...prevalue.final_time, hour_input.value + " : " + min_input.value]
-
-        })
+        let label_input = document.getElementById('label');
+        let pm_am = hour_input.value >= 12 ? " PM" : " AM";
+        setlabel([...label, label_input.value])
+        setvalue(
+            // ...spread operator
+            [...value, hour_input.value + " : " + min_input.value + pm_am]
         )
     }
-    console.log("ft", value.final_time);
-
-
-
-    const [time, setTime] = useState()
-    let Add_alarm_container = []
+    console.log("label", label);
     useEffect(() => {
         setInterval(() => {
             let fetchtime = new Date();
@@ -31,52 +49,47 @@ export default function Home() {
 
             setTime(merge_hour_and_min + " " + pm_am)
         }, 1000)
+    }, [])
+    useEffect(() => {
+        let finded_data = value.filter((item) => item === time)
 
-        Add_alarm_container.push(value.final_time)
-        let finded_alarm = Add_alarm_container[0].filter((item) => item === time);
-        console.log("value", Add_alarm_container);
-        if (Add_alarm_container[0][1] === time) {
-            alert("alarm")
-        }
-    }, [time, value.final_time,Add_alarm_container])
+        value.forEach((data, key) => {
+            if (data === finded_data[0]) {
+                // console.log("key",label[key]);
 
-    console.log(Add_alarm_container);
+
+
+                // console.log("finded_data", index);
+                if (time === finded_data[0]) {
+                    // eslint-disable-next-line
+                    displayNotification(time, key);
+
+                }
+            }
+        })
+    }, [time, value])
     return (
         <>
             <div className='Home'>
-                {/* {CurrentDate} */}
+
+
                 <p>{time}</p>
-
-                <div className='New-button'>
-                    <div>
-                        NEW
-                    </div>
-                </div>
-                <div className='Routines'>
-                    <NavLink to="/SetAlarm" className="navlink">
-                        <h2 className='WT'>Wake up Time <span > &gt;  </span></h2>
-                    </NavLink>
-                    <NavLink to="/SetAlarm" className="navlink">
-                        <h2 className='CT'>College Time <span > &gt;  </span> </h2>
-                    </NavLink>
-                    <NavLink to="/SetAlarm" className="navlink">
-                        <h2 className='LT'>Lunch Time   <span > &gt;  </span> </h2>
-                    </NavLink>
-                    <NavLink to="/SetAlarm" className="navlink">
-                        <h2 className='RT'>Relax Time   <span > &gt;  </span> </h2>
-                    </NavLink>
-                    <NavLink to="/SetAlarm" className="navlink">
-                        <h2 className='BT'>Bed Time     <span > &gt;  </span> </h2>
-                    </NavLink>
-                </div>
             </div>
+            {/* // eslint-disable-next-line */}
+
+            {value.map((Value_item, key) => <div id='Alarm_container'> <h1 className='Alarm'>{Value_item}</h1>
+                <p id={label[key]} className='label'>{label[key]}</p>
+            </div>
+            )}
 
 
-            <h3 >{value.final_time}</h3>
+
+
 
 
 
             <div className='set_alarm_container'>
+                <input placeholder="Enter Your Label" id='label' /><br />
                 <input type="number" id='set_hour' />
                 <input type="number" id='set_min' />
                 <button id='submit' onClick={handle_submit}>submit</button>
